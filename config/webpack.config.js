65b0c75df24afa5b4acf3f0bb68d8fa79694309d
Stephen Roberts <stephen.roberts1@davita.com>
2019-01-27 20:25:32 -0700

    Initial commit

---
+++ config/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    host: '0.0.0.0',
+  },
+  context: path.resolve(__dirname, '..', 'src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, '..', 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.jpg$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './normalize.css';
+import './app.css';
+
+const App = () =>
+  <div className="hero">
+    <h1 className="hero__title">
+      <span className="hero__first-name">
+        <span className="hero__big-letter">S</span>
+        <span className="hero__small-letters">tephen</span>
+      </span>
+      <span className="hero__and">&</span>
+      <span className="hero__second-name">
+        <span className="hero__big-letter">S</span>
+        <span className="hero__small-letters">arah</span>
+      </span>
+    </h1>
+  </div>;
+
+export default App;
