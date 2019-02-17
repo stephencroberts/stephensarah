bb2a41875a52da3b42f74214e983ae66ed6ab68e
Stephen Roberts <stephenroberts@gmail.com>
2019-02-17 14:16:48 -0700

    feat: add hero

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.jpg$/,
+        test: /\.(jpg|png)$/,
         use: ['file-loader'],
       },
     ],
