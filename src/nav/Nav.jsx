81b8fafbdbd1f8ab921298b3642f6c2bc5523321
Stephen Roberts <stephenroberts@gmail.com>
2019-06-09 10:51:11 -0600

    feat(nav): tweak styles and add rsvp link

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -21,18 +21,20 @@ class Nav extends React.Component {
 
   render() {
     return (<div className="nav">
-      <div className="nav__quick-link"><strong><a href="#rsvp">RSVP</a></strong></div>
-      <div className="nav__button">
-        <label htmlFor="nav__checkbox">
-          <i className="fas fa-bars"></i>
-        </label>
-        <input id="nav__checkbox" className="nav__checkbox" type="checkbox" checked={this.state.open} onChange={this.handleMenuClick} />
-        <div className="nav__menu-wrapper">
-          <ul className="nav__menu-items">
-            <li className="nav__menu-item" onClick={this.handleMenuClick}><a href="#home">Home</a></li>
-            <li className="nav__menu-item" onClick={this.handleMenuClick}><a href="#proposal">The Proposal</a></li>
-            <li className="nav__menu-item" onClick={this.handleMenuClick}><a href="#travel">Travel & Transportation</a></li>
-          </ul>
+      <div className="nav__positioner">
+        <div className="nav__quick-link"><strong><a target="_blank" href="https://stephensarah.rsvpify.com">RSVP</a></strong></div>
+        <div className="nav__wrapper">
+          <label className="nav__label" htmlFor="nav__checkbox">
+            <i className="nav__button fas fa-bars"></i>
+          </label>
+          <input id="nav__checkbox" className="nav__checkbox" type="checkbox" checked={this.state.open} onChange={this.handleMenuClick} />
+          <div className="nav__menu-wrapper">
+            <ul className="nav__menu-items">
+              <li className="nav__menu-item" onClick={this.handleMenuClick}><a href="#home">Home</a></li>
+              <li className="nav__menu-item" onClick={this.handleMenuClick}><a href="#proposal">The Proposal</a></li>
+              <li className="nav__menu-item" onClick={this.handleMenuClick}><a href="#travel">Travel & Transportation</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>);
